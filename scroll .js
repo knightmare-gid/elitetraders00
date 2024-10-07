c93e54d6f4d4fb64454df8094a7edfdc1a92c658
knightmare-gid <gidsoala@gmail.com>
2024-10-07 21:13:40 +0100

    Add files via upload

---
+++ scroll .js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeInElements = document.querySelectorAll(".fade-in-section");
+
+    const observerOptions = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("is-visible");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    fadeInElements.forEach(element => {
+        observer.observe(element);
+    });
+});
